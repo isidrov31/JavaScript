27ec85e292401a95b864da3242bd74376fb09215
isidrov31 <127324143+isidrov31@users.noreply.github.com>
2023-06-04 21:14:15 -0300

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,176 @@
-// Variables
+// Manejo de Tablas
 const title = document.querySelectorAll('.titulo');
 const firstTitle = title[0];
 const secondTitle = title[1];
 const thirdTitle = title[2];
-const fourthTitle = title[3];
 const table1 = document.querySelector('.variabletypes');
 const table2 = document.querySelector('.operatortypes');
 const table3 = document.querySelector('.datetypes');
-const table4 = document.querySelector('.what-is-scope');
+
+
+// Manejo de Definicion y Ejemplos
+
+const definition = document.querySelectorAll('#definition');
+const firstDefinition = definition [0];
+const secondDefinition = definition[1];
+const thirdDefinition = definition[2];
+const fourthDefinition = definition[3];
+const fifthDefinition = definition[4];
+const sixthDefinition = definition[5];
+const seventhDefinition = definition[6];
+const eighthDefinition = definition [7];
+
+const example = document.querySelectorAll('#example')
+const firstExample = example [0];
+const secondExample = example[1];
+const thirdExample = example[2];
+const fourthExample = example[3];
+const sixthExample = example[4];
+const seventhExample = example[5];
+const eighthExample = example[6];
+
+const seeDefinition = document.querySelectorAll('.see-definition')
+const firstSeeDefinition = seeDefinition[0];
+const secondSeeDefinition = seeDefinition[1];
+const thirdSeeDefinition= seeDefinition[2];
+const fourthSeeDefinition = seeDefinition[3];
+const fifthSeeDefinition = seeDefinition[4];
+const sixthSeeDefinition = seeDefinition[5];
+const seventhSeeDefinition = seeDefinition[6];
+const eighthSeeDefinition = seeDefinition[7];
+
+const seeExample = document.querySelectorAll('.see-example')
+const firstSeeExample = seeExample[0];
+const secondSeeExample = seeExample[1];
+const thirdSeeExample= seeExample[2];
+const fourthSeeExample = seeExample[3];
+const sixthSeeExample = seeExample[4];
+const seventhSeeExample = seeExample[5];
+const eighthSeeExample = seeExample[6];
+
+
+
+firstDefinition.addEventListener('click', () => {
+    if (firstSeeDefinition.style.display === 'none') {
+        firstSeeDefinition.style.display = 'block';
+    } else { 
+        firstSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+secondDefinition.addEventListener('click', () => {
+    if (secondSeeDefinition.style.display === 'none') {
+        secondSeeDefinition.style.display = 'block';
+    } else { 
+        secondSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+thirdDefinition.addEventListener('click', () => {
+    if (thirdSeeDefinition.style.display === 'none') {
+        thirdSeeDefinition.style.display = 'block';
+    } else { 
+        thirdSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+fourthDefinition.addEventListener('click', () => {
+    if (fourthSeeDefinition.style.display === 'none') {
+        fourthSeeDefinition.style.display = 'block';
+    } else { 
+        fourthSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+fifthDefinition.addEventListener('click', () => {
+    if (fifthSeeDefinition.style.display === 'none') {
+        fifthSeeDefinition.style.display = 'block';
+    } else { 
+        fifthSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+sixthDefinition.addEventListener('click', () => {
+    if (sixthSeeDefinition.style.display === 'none') {
+        sixthSeeDefinition.style.display = 'block';
+    } else { 
+        sixthSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+seventhDefinition.addEventListener('click', () => {
+    if (seventhSeeDefinition.style.display === 'none') {
+        seventhSeeDefinition.style.display = 'block';
+    } else { 
+        seventhSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+eighthDefinition.addEventListener('click', () => {
+    if (eighthSeeDefinition.style.display === 'none') {
+        eighthSeeDefinition.style.display = 'block';
+    } else { 
+        eighthSeeDefinition.style.display = 'none';
+            }
+        }   
+);
+
+firstExample.addEventListener('click', () => {
+    if (firstSeeExample.style.display === 'none') {
+        firstSeeExample.style.display = 'block';
+    } else { 
+        firstSeeExample.style.display = 'none';
+            }
+        }   
+);
+secondExample.addEventListener('click', () => {
+    if (secondSeeExample.style.display === 'none') {
+        secondSeeExample.style.display = 'block';
+    } else { 
+        secondSeeExample.style.display = 'none';
+            }
+        }   
+);
+thirdExample.addEventListener('click', () => {
+    if (thirdSeeExample.style.display === 'none') {
+        thirdSeeExample.style.display = 'block';
+    } else { 
+        thirdSeeExample.style.display = 'none';
+            }
+        }   
+);
+fourthExample.addEventListener('click', () => {
+    if (fourthSeeExample.style.display === 'none') {
+        fourthSeeExample.style.display = 'block';
+    } else { 
+        fourthSeeExample.style.display = 'none';
+            }
+        }   
+);
+sixthExample.addEventListener('click', () => {
+    if (sixthSeeExample.style.display === 'none') {
+        sixthSeeExample.style.display = 'block';
+    } else { 
+        sixthSeeExample.style.display = 'none';
+            }
+        }   
+);
+seventhExample.addEventListener('click', () => {
+    if (seventhSeeExample.style.display === 'none') {
+        seventhSeeExample.style.display = 'block';
+    } else { 
+        seventhSeeExample.style.display = 'none';
+            }
+        }   
+);
+eighthExample.addEventListener('click', () => {
+    if (eighthSeeExample.style.display === 'none') {
+        eighthSeeExample.style.display = 'block';
+    } else { 
+        eighthSeeExample.style.display = 'none';
+            }
+        }   
+);
 
 
 // Manejo de tablas con click
@@ -37,14 +200,6 @@ thirdTitle.addEventListener('click', () => {
     }
 });
 
-fourthTitle.addEventListener('click', () => {
-    if (table3.style.display === 'none') {
-        table3.style.display = 'block';
-    } else {
-        table3.style.display = 'none';
-    }
-});
-
 // Prompt
 const button = document.getElementById('executePrompt');
 button.addEventListener('click',executePrompt);
@@ -71,7 +226,7 @@ function executeAlert(){
 let number1 = document.getElementById('number1');
 let number2 = document.getElementById('number2');
 let add = document.getElementById('add');
-let clean = document.getElementById('clean');
+let clean1 = document.getElementById('clean1');
 let resultDiv = document.querySelector('.resultDiv');
 
 function checkSum(){
@@ -87,8 +242,128 @@ function checkSum(){
 
 add.addEventListener('click', function(){checkSum();})
 
-clean.addEventListener('click', function(){
+clean1.addEventListener('click', function(){
     number1.value = '';
     number2.value = '';
     resultDiv.style = 'transparent'
 })
+
+// Switch
+
+let ageInput = document.getElementById('age');
+let consult = document.getElementById('consult');
+let resultSpan = document.getElementById('result');
+let clean2 = document.getElementById('clean2')
+
+
+consult.addEventListener('click', () => {
+    let age = parseInt(ageInput.value);
+    let result;
+
+switch (age) {
+    case 2024:
+    case 2028: 
+    case 2032:
+    case 2036:
+    case 2040:
+    case 2044:
+    case 2048:
+    case 2052:
+    case 2056:
+    case 2060:
+    case 2064:
+    case 2068:
+    case 2072:
+        result = 'BISIESTO';
+                break;
+    default:
+        result = 'NO BISIESTO'
+                break;
+}
+resultSpan.textContent = result;
+})
+
+clean2.addEventListener('click', () =>{
+    age.value = '';
+    resultSpan.textContent = '';
+})
+
+// For
+
+let clonesInput = document.getElementById('clones');
+let trigger = document.getElementById('trigger');
+let resultClones = document.getElementById('generated-clones');
+let clean3 = document.getElementById('clean3');
+
+trigger.addEventListener('click', () =>{
+       // pasamos a numero el input
+    let clones = parseInt(clonesInput.value); 
+    let result = '';
+
+    for (let i = 0; i < clones; i++){
+        result += 'Generaste un clon. '
+    }
+    resultClones.textContent = result;
+})
+
+clean3.addEventListener('click', () =>{
+    clonesInput.value = '';
+    resultClones.textContent = '';
+})
+
+// Do while
+
+let startButton = document.getElementById('startButton');
+let numberInput = document.getElementById('numberInput');
+let resultDo = document.getElementById('resultDo');
+let clean4 = document.getElementById('clean4');
+
+startButton.addEventListener('click', () => {
+  let number = parseInt(numberInput.value);
+  
+  do {
+    resultDo.textContent += number + '';
+    number += 1;
+  } while (number > 5 && number.toString().length < 2);
+});
+
+clean4.addEventListener('click', () =>{
+    numberInput.value = '';
+    resultDo.textContent = '';
+})
+
+// Array
+
+let naruto = document.getElementsByClassName('naruto');
+let narutos = document.getElementById('narutos');
+let startButtonNaruto = document.getElementById('startButtonNaruto');
+let clean5 = document.getElementById('clean5');
+
+let naruto1 = naruto[0];
+let naruto2 = naruto[1];
+let naruto3 = naruto[2];
+let naruto4 = naruto[3];
+
+
+startButtonNaruto.addEventListener('click', function crearArrayNaruto(naruto1,naruto2,naruto3,naruto4) {
+    let array = [];
+
+    array.push(naruto[0].value);
+    array.push(naruto[1].value);
+    array.push(naruto[2].value);
+    array.push(naruto[3].value);
+    
+    narutos.textContent = JSON.stringify(array);
+
+}
+    )
+
+    clean5.addEventListener('click', () =>{
+        naruto1.value = '';
+        naruto2.value = '';
+        naruto3.value = '';
+        naruto4.value = '';
+        narutos.textContent = '';
+    })
+
+
